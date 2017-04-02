76952cd933c2bb0c6e32e2bb93c55603a1d94bf5
Maxim Tolochko <maksim.tolo@gmail.com>
2017-04-02 15:37:17 +0300

    pipe video from Pi Camera

--- ffmpeg.js
+++ ffmpeg.js
@@ -15,10 +15,6 @@ function FFMPEG(hap, ffmpegOpt) {
   Characteristic = hap.Characteristic;
   StreamController = hap.StreamController;
 
-  if (!ffmpegOpt.source) {
-    throw new Error("Missing source for camera.");
-  }
-
   this.ffmpegSource = ffmpegOpt.source;
   this.ffmpegImageSource = ffmpegOpt.stillImageSource;
 
@@ -233,7 +229,7 @@ FFMPEG.prototype.handleStreamRequest = function(request) {
         let targetVideoPort = sessionInfo["video_port"];
         let videoKey = sessionInfo["video_srtp"];
 
-        let ffmpegCommand = this.ffmpegSource + ' -threads 0 -vcodec libx264 -an -pix_fmt yuv420p -r '+ fps +' -f rawvideo -tune zerolatency -vf scale='+ width +':'+ height +' -b:v '+ bitrate +'k -bufsize '+ bitrate +'k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params '+videoKey.toString('base64')+' srtp://'+targetAddress+':'+targetVideoPort+'?rtcpport='+targetVideoPort+'&localrtcpport='+targetVideoPort+'&pkt_size=1378';
+        let ffmpegCommand = '-re -i /dev/video0 -threads 0 -vcodec libx264 -an -pix_fmt yuv420p -r '+ fps +' -f rawvideo -tune zerolatency -vf scale='+ width +':'+ height +' -b:v '+ bitrate +'k -bufsize '+ bitrate +'k -payload_type 99 -ssrc 1 -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params '+videoKey.toString('base64')+' srtp://'+targetAddress+':'+targetVideoPort+'?rtcpport='+targetVideoPort+'&localrtcpport='+targetVideoPort+'&pkt_size=1378';
         console.log(ffmpegCommand);
         let ffmpeg = spawn('ffmpeg', ffmpegCommand.split(' '), {env: process.env});
         this.ongoingSessions[sessionIdentifier] = ffmpeg;
